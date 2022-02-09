edc9ce55d1c9e77d5dc17ca1d06f725d38c96f79
user <user@DESKTOP-DVT28V8>
2022-02-09 22:31:37 +0900

    OMemberVO-정다원

---
+++ oeProject/src/main/java/kr/member/vo/OMemberVO.java
@@ -0,0 +1,21 @@
+package kr.member.vo;
+
+import java.sql.Date;
+
+public class OMemberVO {
+	private int mem_num;
+	private String mem_id;
+	private int mem_auth;
+	
+	private String mem_nick;
+	private String mem_pw;
+	private String mem_phone;
+	private String mem_photo;
+	private String mem_addr;
+	private String mem_addr2;
+	private String mem_zipcode;
+	private String mem_email;
+	private Date mem_date;
+	private Date mem_modifydate;
+	
+}
