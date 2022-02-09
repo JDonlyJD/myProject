22acc7152301acf0844d607bd15c8f297915a24f
user <user@DESKTOP-DVT28V8>
2022-02-09 22:51:28 +0900

    Merge branch 'main' of https://github.com/JDonlyJD/myProject.git into main

--- oeProject/src/main/java/kr/member/vo/OMemberVO.java
+++ oeProject/src/main/java/kr/member/vo/OMemberVO.java
@@ -17,5 +17,82 @@ public class OMemberVO {
 	private String mem_email;
 	private Date mem_date;
 	private Date mem_modifydate;
-	
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public int getMem_auth() {
+		return mem_auth;
+	}
+	public void setMem_auth(int mem_auth) {
+		this.mem_auth = mem_auth;
+	}
+	public String getMem_nick() {
+		return mem_nick;
+	}
+	public void setMem_nick(String mem_nick) {
+		this.mem_nick = mem_nick;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_phone() {
+		return mem_phone;
+	}
+	public void setMem_phone(String mem_phone) {
+		this.mem_phone = mem_phone;
+	}
+	public String getMem_photo() {
+		return mem_photo;
+	}
+	public void setMem_photo(String mem_photo) {
+		this.mem_photo = mem_photo;
+	}
+	public String getMem_addr() {
+		return mem_addr;
+	}
+	public void setMem_addr(String mem_addr) {
+		this.mem_addr = mem_addr;
+	}
+	public String getMem_addr2() {
+		return mem_addr2;
+	}
+	public void setMem_addr2(String mem_addr2) {
+		this.mem_addr2 = mem_addr2;
+	}
+	public String getMem_zipcode() {
+		return mem_zipcode;
+	}
+	public void setMem_zipcode(String mem_zipcode) {
+		this.mem_zipcode = mem_zipcode;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public Date getMem_date() {
+		return mem_date;
+	}
+	public void setMem_date(Date mem_date) {
+		this.mem_date = mem_date;
+	}
+	public Date getMem_modifydate() {
+		return mem_modifydate;
+	}
+	public void setMem_modifydate(Date mem_modifydate) {
+		this.mem_modifydate = mem_modifydate;
+	}
 }
