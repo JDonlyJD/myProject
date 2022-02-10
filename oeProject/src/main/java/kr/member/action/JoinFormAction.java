d6ffe032bc7a742a0c8b7166ef326a20f7768562
user <user@DESKTOP-VMEID54>
2022-02-10 17:05:03 +0900

    joinForm,joinFormAction

--- oeProject/src/main/java/kr/member/action/JoinFormAction.java
+++ oeProject/src/main/java/kr/member/action/JoinFormAction.java
@@ -1,5 +1,16 @@
 package kr.member.action;
 
-public class JoinFormAction {
-  
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.controller.Action;
+
+public class JoinFormAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		//JSP경로 반환
+		return "/WEB-INF/views/member/joinForm.jsp";
+	}
 }
