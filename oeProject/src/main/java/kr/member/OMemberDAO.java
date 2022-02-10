b929ae22f439bb44f31af2d6f5ff4c3b3473cc4b
user <user@DESKTOP-VMEID54>
2022-02-10 16:38:18 +0900

    dao_찬미

--- oeProject/src/main/java/kr/member/OMemberDAO.java
+++ oeProject/src/main/java/kr/member/OMemberDAO.java
@@ -17,10 +17,124 @@ public class OMemberDAO {
 	}
 	private OMemberDAO() {}	
 	
-	// 찬미
-	// [메서드1. 회원가입 : inseretMember() : 로그인 + 아이디 중복체크]
-	// [메서드2. ID중복체크 및 로그인 처리 :  checkMember() : 로그인 + 아이디 중복체크]
-	
+		// 찬미
+		// [메서드1. 회원가입 : inseretMember() : 로그인 + 아이디 중복체크]
+		public void insertMember(OMemberVO member)throws Exception{
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			PreparedStatement pstmt2 = null;
+			PreparedStatement pstmt3 = null;
+			ResultSet rs = null;
+			String sql = null;
+			int num = 0; //시퀀스 번호 저장
+			
+			try {
+				//커넥션풀로부터 커넥션 할당
+				conn = DBUtil.getConnection();
+				//★ 오토 커밋 해제(sql문이 2개 이상일때)
+				conn.setAutoCommit(false);
+				
+				//회원번호(mem_num) 생성
+				sql = "SELECT omember_seq.nextval FROM dual"; //동일한 시퀀스를 구하기 위해 별도의 sql문
+				//PreparedStatement객체 생성
+				pstmt = conn.prepareStatement(sql);
+				//sql문을 실행시켜 결과행을 rs담음
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					num = rs.getInt(1);
+				}
+				
+				//omember테이블에 데이터 저장(auth값은 디폴트값 1로 들어가므로 생략o)
+				sql = "INSERT INTO omember (mem_num,mem_id) VALUES (?,?)";
+				//PreparedStatement객체 생성
+				pstmt2 = conn.prepareStatement(sql);
+				//?에 데이터 바인딩
+				pstmt2.setInt(1, num);
+				pstmt2.setString(2, member.getMem_id());
+				//sql문 실행
+				pstmt2.executeUpdate();
+				
+				//omember_detail 테이블에 데이터 저장
+				sql = "INSERT INTO omember_detail (mem_num,mem_id,mem_nick,mem_pw,mem_phone,mem_email,"
+						+ "mem_zipcode,mem_addr,mem_addr2) VALUES (?,?,?,?,?,?,?,?,?)";
+				//PreparedStatement객체 생성
+				pstmt3 = conn.prepareStatement(sql);
+				//?에 데이터 바인딩
+				pstmt3.setInt(1, num);
+				pstmt3.setString(2, member.getMem_id());
+				pstmt3.setString(3, member.getMem_nick());
+				pstmt3.setString(4, member.getMem_pw());
+				pstmt3.setString(5, member.getMem_phone());
+				pstmt3.setString(6, member.getMem_email());
+				pstmt3.setString(7, member.getMem_zipcode());
+				pstmt3.setString(8, member.getMem_addr());
+				pstmt3.setString(9, member.getMem_addr2());
+				
+				//sql문 실행
+				pstmt3.executeUpdate();
+				
+				//★ sql문 실행시 모두 성공하면 commit
+				conn.commit();
+				
+			}catch(Exception e) {
+				//★ sql문이 하나라도 실패하면 롤백(rollback)작업
+				conn.rollback();
+				
+				throw new Exception(e);
+			}finally {
+				//자원정리
+				DBUtil.executeClose(null, pstmt3, null);
+				DBUtil.executeClose(null, pstmt2, null);
+				DBUtil.executeClose(rs, pstmt, conn);
+			}
+		}
+		
+		// [메서드2. ID중복체크 및 로그인 처리 :  checkMember() : 로그인 + 아이디 중복체크]
+		public OMemberVO checkMember(String mem_id)throws Exception{
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			OMemberVO member = null;
+			String sql = null;
+			
+			try {
+				//1.커넥션풀로부터 커넥션 할당
+				conn = DBUtil.getConnection();
+				
+				//2.sql문 작성
+				//omember와 omember_detail테이블을 조인. omember의 누락된 데이터가 보여야
+				//id중복 체크 가능
+				sql = "SELECT * FROM omember m LEFT OUTER JOIN omember_detail d "
+						+ "ON m.mem_num=d.mem_num WHERE m.mem_id=?";
+				
+				//3.PreparedStatement객체 생성
+				pstmt = conn.prepareStatement(sql);
+				
+				//4.?에 데이터 바인딩
+				pstmt.setString(1, member.getMem_id());
+				
+				//5.sql문을 실행시켜 결과행을 rs에 담아 반환
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					member = new OMemberVO();
+					
+					member.setMem_num(rs.getInt("mem_num"));
+					member.setMem_id(rs.getString("mem_id"));
+					member.setMem_nick(rs.getString("mem_nick")); //nick = name이름
+					member.setMem_auth(rs.getInt("mem_auth"));
+					member.setMem_pw(rs.getString("mem_pw"));
+					member.setMem_photo(rs.getString("mem_photo"));
+				}
+				
+			}catch(Exception e) {
+				throw new Exception(e);
+			}finally {
+				//자원정리
+				DBUtil.executeClose(rs, pstmt, conn);
+			}
+			return member;
+		}
 	
 	// 다원
 	// [메서드3. ID찾기 : findIdMember()]
