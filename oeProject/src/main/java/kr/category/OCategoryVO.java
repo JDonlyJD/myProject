cd4ae5b76c1c5c4773d121862a2c6d0914dfe732
minjeongHEO <hmjoung9770@gmail.com>
2022-02-20 17:30:00 +0900

    카테고리

--- oeProject/src/main/java/kr/category/OCategoryVO.java
+++ oeProject/src/main/java/kr/category/OCategoryVO.java
@@ -1,9 +1,22 @@
 package kr.category;
 
 public class OCategoryVO {
-	private int cate_num; //카테고리 번호
-	private int cate_name; //카테고리 이름
+	private int cate_num; //카테고리 번호 (PK,NOT NULL)
+	private String cate_name; //카테고리 이름 (NOT NULL)
 	private int cate_status; //(0미사용, 1사용)
+	
+	
+	
+//	private int mem_num;//회원번호
+//	
+//	public int getMem_num() {
+//		return mem_num;
+//	}
+//	public void setMem_num(int mem_num) {
+//		this.mem_num = mem_num;
+//	}
+	
+	
 	
 	public int getCate_num() {
 		return cate_num;
@@ -11,10 +24,10 @@ public class OCategoryVO {
 	public void setCate_num(int cate_num) {
 		this.cate_num = cate_num;
 	}
-	public int getCate_name() {
+	public String getCate_name() {
 		return cate_name;
 	}
-	public void setCate_name(int cate_name) {
+	public void setCate_name(String cate_name) {
 		this.cate_name = cate_name;
 	}
 	public int getCate_status() {
